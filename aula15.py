6e8b8797810b49c22093ee272f15cee27d1840b6
Zenir <herbert@grupozenir.com.br>
2024-05-03 14:18:15 -0300

    [Add] Adicionado novo script com gitignore

---
+++ aula15.py
@@ -0,0 +1,9 @@
+# if / elif / else
+# se / se não se / se não
+
+condicao = True
+
+if condicao: # se a condição for falsa ele pula essa condição
+    print('Este é o if')
+else:
+    print('Este é o else do if')
